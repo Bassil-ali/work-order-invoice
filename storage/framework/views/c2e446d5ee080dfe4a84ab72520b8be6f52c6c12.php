750c066e6007404736e97c1908366c38d29b7c2c
codexSD <80596282+codexSD@users.noreply.github.com>
2024-11-11 15:24:00 +0300

    fixing

--- storage/framework/views/c2e446d5ee080dfe4a84ab72520b8be6f52c6c12.php
+++
@@ -1,8 +0,0 @@
-<?php echo $__env->make('admin.layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->make('admin.layouts.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->make('admin.layouts.menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->make('admin.layouts.messages', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-<?php echo $__env->yieldContent('content'); ?>
-
-<?php echo $__env->make('admin.layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Bassil-Ali\Desktop\work-order\resources\views/admin/index.blade.php ENDPATH**/ ?>
